7e861f8aefe4462bcb71799082826d110c791760
Omobolaji Arigbede <arigbedeomobolaji@gmail.com>
2023-11-25 18:05:40 +0100

    created useMovie Hook

--- client/src/components/MovieList.jsx
+++ client/src/components/MovieList.jsx
@@ -1,16 +1,8 @@
-/* eslint-disable react/jsx-key */
-import { useContext, useEffect, useState } from "react";
 import MovieCard from "./MovieCard";
-import { MovieContext } from "../reducers/MovieContext";
+import useMovies from "../hooks/useMovies";
 
 function MovieList() {
-	const state = useContext(MovieContext);
-	const [movies, setMovies] = useState(state.movies);
-
-	useEffect(() => {
-		setMovies(state.movies);
-	}, [state.movies]);
-
+	const movies = useMovies();
 	return (
 		<div className="flex flex-col pb-5">
 			<div className="max-w-7xl mx-3 my-5 lg:mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 justify-items-start md:justify-items-center">
