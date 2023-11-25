75f05c2874d90f5ef65923b94cf663267e12d3c3
Omobolaji Arigbede <arigbedeomobolaji@gmail.com>
2023-11-25 17:54:37 +0100

    added commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,13 +6,19 @@ import { useReducer } from "react";
 import { MovieContext, MovieDispatchContext } from "./reducers/MovieContext";
 
 export default function App() {
+	// reducer for fetching and and working with the movies data
 	const { movieReducer } = useMoviesHooks();
+	// useReducer to manage the state and action that can be performed on the movies data
 	const [state, dispatch] = useReducer(movieReducer, initialMovieState);
+	// I created Two context so as to avoid props drilling
 	return (
 		<MovieContext.Provider value={state}>
 			<MovieDispatchContext.Provider value={dispatch}>
+				{/* Navbar */}
 				<Navbar />
+				{/* Filter Section */}
 				<Filter />
+				{/* MovieList Section */}
 				<MovieList />
 			</MovieDispatchContext.Provider>
 		</MovieContext.Provider>
